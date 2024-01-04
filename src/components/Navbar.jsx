8fd27ab69863c9a36314848b48a65046c4444740
perimellisowmya <perimellisowmya@gmail.com>
2024-01-04 22:41:31 +0530

    Redirect URLs Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 
 function Navbar() {
   const navItems = [
-    { link: 'Overview', path: 'home' },
-    { link: 'Feature', path: 'feature' },
-    { link: 'About', path: 'about' },
-    { link: 'Pricing', path: 'pricing' },
+    { link: 'Overview'},
+    { link: 'Feature'},
+    { link: 'About' },
+    { link: 'Pricing' },
   ];
   return (
     <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between bg-white p-4">
       <h2 className="text-8xl lg:text-5xl font-bold  text-black">soarX</h2>
       <ul className="flex flex-col lg:flex-row lg:space-x-12 lg:mt-0 mt-4">
-        {navItems.map(({ link, path }) => (
+        {navItems.map(({ link }) => (
           <li key={link}>
-            <a href={path} className="text-black hover:text-gray-300 block lg:inline-block">
+            <a className="text-black hover:text-gray-300 block lg:inline-block">
               {link}
             </a>
           </li>
